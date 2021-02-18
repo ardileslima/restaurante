fd56350d44fa1866eb74f31a6a9044e0def5f9f8
ardileslima <ardileslima@gmail.com>
2021-02-18 17:11:45 -0300

    primeiro commit

---
+++ javascript/produtos.js
@@ -0,0 +1,78 @@
+const produtos = [
+    {
+        id: 1,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 01',
+        description: 'Descrição do produto 01',
+        value: 10.00
+    },
+
+    {
+        id: 2,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 02',
+        description: 'Descrição do produto 02',
+        value: 23.00
+    },
+
+    {
+        id: 3,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 03',
+        description: 'Descrição do produto 03',
+        value: 13.00
+    },
+
+    {
+        id: 4,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 04',
+        description: 'Descrição do produto 04',
+        value: 12.00
+    },
+    {
+        id: 5,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 05',
+        description: 'Descrição do produto 05',
+        value: 12.00
+    },
+    {
+        id: 6,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 06',
+        description: 'Descrição do produto 06',
+        value: 12.00
+    },
+    {
+        id: 7,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 07',
+        description: 'Descrição do produto 07',
+        value: 12.00
+    },
+    {
+        id: 8,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 08',
+        description: 'Descrição do produto 08',
+        value: 12.00
+    },
+    {
+        id: 9,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 09',
+        description: 'Descrição do produto 09',
+        value: 12.00
+    },
+
+    {
+        id: 10,
+        image: 'https://picsum.photos/200',
+        title: 'Produto 10',
+        description: 'Descrição do produto 10',
+        value: 12.00
+    }
+    
+    
+]
